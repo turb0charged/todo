7edb9c6cee6c45448d4159a8eba28782a3aa32f4
hector castillo <hc267@afsconnect1.njit.edu>
2014-03-25 10:27:48 -0400

    setting the default due date

--- js/models/todo.js
+++ js/models/todo.js
@@ -10,7 +10,8 @@ define([
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			setDate: currentDate.getMonth()+1 + '/' + currentDate.getDate() + '/' + currentDate.getFullYear()
 		},
 
 		// Toggle the `completed` state of this todo item.
